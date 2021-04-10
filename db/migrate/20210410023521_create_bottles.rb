d61d91d3f65c0bbbf2751b1ebf405b874395dc6f
imcarissa <carissa.sarreal@gmail.com>
2021-04-09 22:36:23 -0400

    resource g bottle

---
+++ db/migrate/20210410023521_create_bottles.rb
@@ -0,0 +1,15 @@
+class CreateBottles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bottles do |t|
+      t.string :name
+      t.string :type
+      t.integer :vintage
+      t.string :region
+      t.string :review
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
