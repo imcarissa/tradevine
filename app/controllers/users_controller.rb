87c3928db6b91635fa1a558523a95c0f4e1c4df3
imcarissa <carissa.sarreal@gmail.com>
2021-04-10 02:37:55 -0400

    fixed route for the redirect in create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,22 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
           session[:user_id] = @user.id
-          redirect_to @user
+          redirect_to user_path(@user)
         else
           render :new
         end
     end
 
     def show 
-        
+      @user = User.find_by_id(params[:id])
+    end
+
+    def edit
+
+    end
+
+    def update
+
     end
 
     private
