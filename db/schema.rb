09f7f0359d995e82e881e8c7aa430b0d6941599e
imcarissa <carissa.sarreal@gmail.com>
2021-04-10 02:50:31 -0400

    description column added to bottles tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_035414) do
+ActiveRecord::Schema.define(version: 2021_04_10_064555) do
 
   create_table "bottles", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_04_10_035414) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
     t.index ["category_id"], name: "index_bottles_on_category_id"
     t.index ["user_id"], name: "index_bottles_on_user_id"
   end
