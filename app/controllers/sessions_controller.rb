6e2cefad6a5c8ecac8c991feca0da5021698bdc0
imcarissa <carissa.sarreal@gmail.com>
2021-04-10 00:23:00 -0400

    creates a session for a user upon being logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 class SessionsController < ApplicationController
 
+
+    def create
+        user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate
+            session[:user.id] = user.id
+            redirect_to user_path(user)
+        else
+            redirect_to "/login"
+        end
+    end
+
+
     def destroy
         session.clear
         redirect_to root_path
