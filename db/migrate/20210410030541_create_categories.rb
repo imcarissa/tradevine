0b947be534efee0fa57cee9c1cbec3345340bbe0
imcarissa <carissa.sarreal@gmail.com>
2021-04-09 23:12:57 -0400

    removed null: false from t.references line

--- db/migrate/20210410030541_create_categories.rb
+++ db/migrate/20210410030541_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
       t.string :name
-      t.references :user, null: false, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
