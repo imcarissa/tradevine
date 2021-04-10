26219887e34be5ebaa0ebc8059588d99a04a817c
imcarissa <carissa.sarreal@gmail.com>
2021-04-09 22:58:30 -0400

    add associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :bottles
+    has_many :reviews
+    has_many :reviewed_posts, through: :reviews, source: :bottle
+    has_many :categories, through: :bottles
 end
