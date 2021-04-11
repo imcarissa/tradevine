774c8ed846fc40f09abaa012685e96f75bb3f4da
imcarissa <carissa.sarreal@gmail.com>
2021-04-11 01:48:52 -0400

    changed column names from type to wine type

--- db/migrate/20210410023521_create_bottles.rb
+++ db/migrate/20210410023521_create_bottles.rb
@@ -1,11 +1,12 @@
 class CreateBottles < ActiveRecord::Migration[6.1]
   def change
     create_table :bottles do |t|
-      t.string :name
-      t.string :type
+      t.string :wine_name
+      t.string :wine_type
       t.integer :vintage
       t.string :region
-      t.string :notes
+      t.string :t_notes
+      t.string :comments
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :category, null: false, foreign_key: true
 
