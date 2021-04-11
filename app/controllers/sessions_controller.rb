8d076d88c991b0b5ba5acb6afc2ccd1913ebb3c6
imcarissa <carissa.sarreal@gmail.com>
2021-04-11 02:37:55 -0400

    authenticate through password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
 
 
-    def create
-        user = User.find_by(username: params[:user][:username])
-        if user && user.authenticate(params[:user][:password])
-          session[:user_id] = user.id
-          redirect_to user_path(user)
-        else
-          flash[:message] = "Incorrect login info, please try again"
-          redirect_to "/login"
-        end
+    def create 
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        flash[:message] = "Wrong login info"
+        redirect_to "/login"
       end
+    end
 
 
     def destroy
