369c9cfdf84a7aa3b0707c9223bf67d8b686492a
imcarissa <carissa.sarreal@gmail.com>
2021-04-11 01:42:27 -0400

    added index and show action, corrected bottles build in create action

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -2,17 +2,25 @@ class BottlesController < ApplicationController
     before_action :redirect_if_not_logged_in
 
 
+    def index
+        @bottles = Bottle.all
+    end
+
     def new
         @bottle = Bottle.new 
     end
 
     def create
-        @bottle = current_user.bottle.build(bottle_params)
+        @bottle = current_user.bottles.build(bottle_params)
         if @bottle.save
             redirect_to bottles_path
         else
             render :new
         end
+    end
+
+    def show
+        @bottle = Bottle.find_by_id(params[:id])
     end
 
     private
