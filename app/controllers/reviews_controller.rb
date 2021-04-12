d08ccbc86d5980a13d1061f15bf29e3011ed8ae1
imcarissa <carissa.sarreal@gmail.com>
2021-04-12 07:28:29 -0400

    added if else to index action to be able to review a bottle

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,21 +2,27 @@ class ReviewsController < ApplicationController
     before_action :redirect_if_not_logged_in
 
     def index
-        @reviews = Review.all
-      end
-  
-      def new
-        @review = Review.new
-      end
-  
-      def create
-        @review = current_user.reviews.build(review_params)
-          if @review.save
-            redirect_to review_path
-          else
-            redirect_to reviews_path
-          end
+        if params[:bottle_id] && @bottle = Bottle.find_by_id(params[:bottle_id])
+          @reviews = @bottle.comments 
+        else
+          flash[:errors] = "bottle does not exist" if params[:bottle_id]
+          @reviews = Review.all
         end
+    end
+  
+  
+    def new
+        @review = Review.new
+    end
+  
+    def create
+        @review = current_user.reviews.build(review_params)
+        if @review.save
+            redirect_to review_path
+        else
+            redirect_to bottles_path
+        end
+    end
   
       def show
         @review = Review.find_by(id: params[:id])
@@ -35,6 +41,5 @@ class ReviewsController < ApplicationController
       def review_params 
           params.require(:review).permit(:content)
       end
+
   end
-  
-end
