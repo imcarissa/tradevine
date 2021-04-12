4dacc4d0a74dd5c4702209ef2ad038d5ab1a9c66
imcarissa <carissa.sarreal@gmail.com>
2021-04-12 11:29:31 -0400

    deleted migration

--- db/migrate/20210412105142_remove_wine_type_from_bottles.rb
+++
@@ -1,8 +0,0 @@
-class RemoveWineTypeFromBottles < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :bottles, :wine_type, :string
-    remove_column :bottles, :vintage, :integer
-    remove_column :bottles, :comments, :string
-    remove_column :bottles, :region, :string
-  end
-end
