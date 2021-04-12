365155a527be9142718c5b7154aca17abab6b0fc
imcarissa <carissa.sarreal@gmail.com>
2021-04-12 06:44:12 -0400

    nested routes for users and bottles

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,15 @@ Rails.application.routes.draw do
   get '/logout', to: "sessions#destroy"
 
   resources :categories
+  
   resources :reviews
-  resources :users
-  resources :bottles
+  
+  resources :users do
+    resources :posts, only: [:new, :create, :index]
+  end
+  
+  resources :bottles do
+    resources :reviews, only [:new, :create, :index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
