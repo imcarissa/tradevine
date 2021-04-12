1173325c520e690f8da6f1db35e1c58e1a31f2cf
imcarissa <carissa.sarreal@gmail.com>
2021-04-12 11:29:14 -0400

    changed column names

--- db/migrate/20210410023521_create_bottles.rb
+++ db/migrate/20210410023521_create_bottles.rb
@@ -2,11 +2,8 @@ class CreateBottles < ActiveRecord::Migration[6.1]
   def change
     create_table :bottles do |t|
       t.string :wine_name
-      t.string :wine_type
-      t.integer :vintage
       t.string :region
-      t.string :t_notes
-      t.string :comments
+      t.string :description
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :category, null: false, foreign_key: true
 
