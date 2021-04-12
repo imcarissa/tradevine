9ea988838b1856e5311cb2a615d861d4c624b3aa
imcarissa <carissa.sarreal@gmail.com>
2021-04-12 07:27:44 -0400

    forgot a : sign after an only

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   end
   
   resources :bottles do
-  #  resources :reviews, only [:new, :create, :index]
+    resources :reviews, only: [:new, :create, :index]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
