2f4fbd8bef2796ea98005b91f19737a1a695c930
imcarissa <carissa.sarreal@gmail.com>
2021-04-12 06:40:10 -0400

    changed redirect to login_path from rendr new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
         session[:user_id] = user.id
         redirect_to user_path(user)
       else
-        flash[:message] = "Wrong login info"
+        flash[:errors] = "Incorrect login info. Please try again."
         redirect_to login_path
       end
     end
