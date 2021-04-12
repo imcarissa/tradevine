5e5f325d353b174d3784df8283384fd590b99b7c
imcarissa <carissa.sarreal@gmail.com>
2021-04-12 11:10:04 -0400

    removed region column from bottles table

--- db/migrate/20210412105142_remove_wine_type_from_bottles.rb
+++ db/migrate/20210412105142_remove_wine_type_from_bottles.rb
@@ -3,5 +3,6 @@ class RemoveWineTypeFromBottles < ActiveRecord::Migration[6.1]
     remove_column :bottles, :wine_type, :string
     remove_column :bottles, :vintage, :integer
     remove_column :bottles, :comments, :string
+    remove_column :bottles, :region, :string
   end
 end
