94ffc10095a111900571ee9a266d242736a37bb5
imcarissa <carissa.sarreal@gmail.com>
2021-04-12 06:56:51 -0400

    created CRUD for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,40 @@
 class ReviewsController < ApplicationController
     before_action :redirect_if_not_logged_in
 
-    
+    def index
+        @reviews = Review.all
+      end
+  
+      def new
+        @review = Review.new
+      end
+  
+      def create
+        @review = current_user.reviews.build(review_params)
+          if @review.save
+            redirect_to review_path
+          else
+            redirect_to reviews_path
+          end
+        end
+  
+      def show
+        @review = Review.find_by(id: params[:id])
+      end
+  
+      def edit
+        @review = Review.find_by(id: params[:id])
+      end
+  
+      def update 
+      
+      end
+  
+        private
+  
+      def review_params 
+          params.require(:review).permit(:content)
+      end
+  end
+  
 end
