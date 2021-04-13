e5f4d84450d240a7d293b49ce16622853d93e949
imcarissa <carissa.sarreal@gmail.com>
2021-04-12 22:58:11 -0400

    added columns to bottles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_035414) do
+ActiveRecord::Schema.define(version: 2021_04_13_024848) do
 
   create_table "bottles", force: :cascade do |t|
     t.string "wine_name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_04_10_035414) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "vintage"
+    t.string "winery"
     t.index ["category_id"], name: "index_bottles_on_category_id"
     t.index ["user_id"], name: "index_bottles_on_user_id"
   end
