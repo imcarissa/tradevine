74931b26c04fe0d2797da14275057f143e41297b
imcarissa <carissa.sarreal@gmail.com>
2021-04-13 17:10:49 -0400

    added validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
     belongs_to :user
     belongs_to :bottle
+   
+    validates :content, presence: true
+    validates :user_id, presence: true
+    validates :post_id, presence: true
 end
