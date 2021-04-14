68e63bb4cc64a479ec903db25ecc167a86253125
imcarissa <carissa.sarreal@gmail.com>
2021-04-14 13:46:57 -0400

    private method for omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
     has_many :bottles
     has_many :reviews
-    has_many :reviewed_posts, through: :reviews, source: :bottle
+    has_many :reviewed_bottles, through: :reviews, source: :bottle
     has_many :categories, through: :bottles
 
     has_secure_password
+
+    def self.find_with_omniauth(auth)
+        binding.pry
+    end
+
+
 end
