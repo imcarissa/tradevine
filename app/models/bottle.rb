cee9f58b43f1afc32ce614193c867bb97e6a85ff
imcarissa <carissa.sarreal@gmail.com>
2021-04-15 18:16:37 -0400

    accepts nested attributes

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -4,11 +4,13 @@ class Bottle < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
   validates :wine_name, :vintage, :winery, :region, :description, presence: true
+  accepts_nested_attributes_for :category
 
   scope :alpha, -> { order(:wine_name) }
  # scope :most_comments, -> joins(:reviews).group('bottles.id').order('count(bottles.id) desc')
   
   def category_attributes=(attr)
+  
     self.category = Category.find_or_create_by(attr) if !attr[:name].blank?
   end
 
