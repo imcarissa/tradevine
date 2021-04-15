a41807f39e7cce700e43733b6610d1ef75270b05
imcarissa <carissa.sarreal@gmail.com>
2021-04-15 05:31:51 -0400

    added validations

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -3,10 +3,10 @@ class Bottle < ApplicationRecord
   belongs_to :category
   has_many :reviews
   has_many :users, through: :reviews
-  validates :content, :title, presence: true
+  validates :wine_name, :vintage, :winery, :region, :description, presence: true
 
   scope :alpha, -> { order(:wine_name) }
- # scope :most_comments, -> joins(:reviews).group(‘bottles.id’).order(‘count(bottles.id) desc’)
+ # scope :most_comments, -> joins(:reviews).group('bottles.id').order('count(bottles.id) desc')
   
   def category_attributes(attr)
     self.category = Category.find_or_create_by(attr) if !attr[:name].blank?
