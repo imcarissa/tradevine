5d67d8fe07b169f8540998bd7513e4b437791ef8
imcarissa <carissa.sarreal@gmail.com>
2021-04-15 04:03:55 -0400

    alpha scope added

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -4,5 +4,11 @@ class Bottle < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
   validates :content, :title, presence: true
+
+  scope :alpha, -> { order(:wine_name)}
   
+  def category_attributes(attr)
+    self.category = Category.find_or_create_by(attr) if !attr[:name].blank?
+  end
+
 end
