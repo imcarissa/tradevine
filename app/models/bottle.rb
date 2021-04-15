fb2bcb569b7b5f3a98e0f89973526e4ea96dcfe6
imcarissa <carissa.sarreal@gmail.com>
2021-04-15 16:47:38 -0400

    fixed missing = sign for category attributes=attr

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -8,7 +8,7 @@ class Bottle < ApplicationRecord
   scope :alpha, -> { order(:wine_name) }
  # scope :most_comments, -> joins(:reviews).group('bottles.id').order('count(bottles.id) desc')
   
-  def category_attributes(attr)
+  def category_attributes=(attr)
     self.category = Category.find_or_create_by(attr) if !attr[:name].blank?
   end
 
