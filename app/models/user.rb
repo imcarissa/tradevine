68f769a68950cbc6570b86c1dc17eb47f246842a
imcarissa <carissa.sarreal@gmail.com>
2021-04-14 21:11:44 -0400

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_bottles, through: :reviews, source: :bottle
     has_many :categories, through: :bottles
-
+    
     has_secure_password
-
+    validates :username, :email, presence: true
+    
     def self.find_with_omniauth(auth)
-        binding.pry
+     
     end
 
 
