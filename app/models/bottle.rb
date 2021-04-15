70736f34c89a673e16edaa79e95c5321bd1303e2
imcarissa <carissa.sarreal@gmail.com>
2021-04-15 04:42:09 -0400

    most_comments scope added

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -5,7 +5,8 @@ class Bottle < ApplicationRecord
   has_many :users, through: :reviews
   validates :content, :title, presence: true
 
-  scope :alpha, -> { order(:wine_name)}
+  scope :alpha, -> { order(:wine_name) }
+  scope :most_comments, -> joins(:reviews).group(‘bottles.id’).order(‘count(bottles.id) desc’)
   
   def category_attributes(attr)
     self.category = Category.find_or_create_by(attr) if !attr[:name].blank?
