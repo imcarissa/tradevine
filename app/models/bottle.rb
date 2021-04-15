04da062a11998bfd30d71fad11320b78cf2e308c
imcarissa <carissa.sarreal@gmail.com>
2021-04-14 21:12:46 -0400

    added validations

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -1,5 +1,9 @@
 class Bottle < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
+  belongs_to :category
   has_many :reviews
   has_many :users, through: :reviews
+  validates :content, :title, presence: true
+  validate :too_many_posts
+  
 end
