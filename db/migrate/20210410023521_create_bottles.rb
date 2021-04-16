1b7d33c54faae0d57f4023c4733dc649d77770aa
imcarissa <carissa.sarreal@gmail.com>
2021-04-16 16:39:22 -0400

    create bottles table in schema

--- db/migrate/20210410023521_create_bottles.rb
+++ db/migrate/20210410023521_create_bottles.rb
@@ -2,6 +2,7 @@ class CreateBottles < ActiveRecord::Migration[6.1]
   def change
     create_table :bottles do |t|
       t.string :wine_name
+      t.integer :vintage
       t.string :region
       t.string :description
       t.belongs_to :user, null: false, foreign_key: true
