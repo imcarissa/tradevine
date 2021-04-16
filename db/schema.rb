6d137eff35567d20c1656c9c690b51c0dd6700cf
imcarissa <carissa.sarreal@gmail.com>
2021-04-15 21:51:15 -0400

    no changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_165754) do
+ActiveRecord::Schema.define(version: 2021_04_16_014412) do
 
   create_table "bottles", force: :cascade do |t|
     t.string "wine_name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_165754) do
     t.string "winery"
     t.index ["category_id"], name: "index_bottles_on_category_id"
     t.index ["user_id"], name: "index_bottles_on_user_id"
+    t.index ["wine_name"], name: "index_bottles_on_wine_name"
   end
 
   create_table "categories", force: :cascade do |t|
